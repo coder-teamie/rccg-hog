931fe8563b4d5e857a4f95d76340a3ac816aebaf
teamie <timmieprince@gmail.com>
2022-04-09 01:02:07 +0800

    navigation bar complete!

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+const navToggle = document.getElementById('nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+const socialIcons = document.querySelector('.social-icons');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show-links');
+  socialIcons.classList.toggle('show-links');
+});
