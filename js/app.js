669c5d330b2efa5307c97810fbaf7d88887fb29d
teamie <timmieprince@gmail.com>
2022-04-12 23:59:11 +0800

    weekly word added + minor design changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+// *** Navigation *** // more work to come..
 const navToggle = document.getElementById('nav-toggle');
-// const navLinks = document.querySelector('.nav-links');
-// const socialIcons = document.querySelector('.social-icons');
 const linksContainer = document.querySelector('.links-container');
 
 navToggle.addEventListener('click', () => {
   linksContainer.classList.toggle('show-links');
 });
+
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear());
